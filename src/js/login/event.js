42a4d5e500fe6e8ea1fe26cc564bbd722423844c
Chris <88316842@qq.com>
2018-03-07 15:53:36 +0800

    # 登页模拟接口完成

--- src/js/login/event.js
+++ src/js/login/event.js
@@ -1,5 +1,27 @@
 import formCheck from '../common/formCheck.js'
+import {$} from '../common/utils.js'
+import {fetchPost} from "../common/fetch"
+
 export default () => {
+    const  $loginFrom = $('login-form')
+    const $loginBtn = $('login-btn')
+    const $clearAccount = $('clear-account')
+    const $remember = $('login-remember')
+    const $account = $('login-account')
+    const $password = $('login-password')
+    $loginFrom.onsubmit = async (e) => {
+        e.preventDefault(e)
+        let remember = '0'
+        if ($remember.getAttribute('checked')) {
+            remember = '1'
+        }
+        const data = await fetchPost('/login',{
+            account: $account.value,
+            password: $password.value,
+            remember:remember
+        })
+        console.log(data)
+    }
     // let btn = document.getElementById('submit')
     // let input = document.getElementById('input')
     // const check = formCheck(document.getElementById('form'))
--- src/js/login/render.js
+++ src/js/login/render.js
@@ -5,21 +5,37 @@ const template = (opts = {}) => {
                  <input type="text">
                   <input type="password">
           </div>`
-    const autoCompleteAdapter = opts.autoComplete ? '' : autoCompleteTpl
+    const showRemember = opts.showRemember ? 'block' : 'none';
+    const autocompleteAdapter = opts.autoComplete ? '' : autoCompleteTpl
+    const autocompleteValue = opts.autocomplete ? 'on' : 'off';
     const tpl = `
     <div id="login-wrapper">
-            <form id="login-form" onsubmit="return false">
-                ${autoCompleteAdapter}
-                <label class="login-accout-wrapper">
-                    <span class="account-label">${opts.accountLabel}</span>
-                    <input id="login-account" name="account" type="text" placeholder="${opts.accountPlaceHolder}" autocomplete="off">
+            <p id="login-error" class="login-error"></p>
+                   <form id="login-form" onsubmit="return false">
+                ${ autocompleteAdapter }
+                <label class="login-accound-wrapper">
+                    <span class="account-label">${ opts.accountLabel }</span>
+                    <input id="login-account" valid="present" name="account" type="text" placeholder="${ opts.accountPlaceHolder }" maxlength="${opts.accountMax}" autocomplete="${ opts.autocompleteValue }">
                     <span id="clear-account" class="del"></span>
-                </label>    
-                <label class="login-accout-wrapper">
-                    <span class="password-label">${opts.passwordLabel}</span>
-                    <input id="login-password" name="password" type="password" autocomplete="off" placeholder="${opts.passwordPlaceHolder}">
                 </label>
-                <input id="login-btn" class="login-btn" type="submit" value="${opts.loginBtnText}">         
+
+                <label class="login-password-wrapper">
+                    <span class="password-label">${ opts.passwordLabel }</span>
+                    <input id="login-password" valid="present" name="password" type="password" placeholder="${ opts.passwordPlaceHolder }" maxlength="${opts.passwordMax}" autocomplete="${ autocompleteValue }">
+                </label>
+
+                <label style="display: none" class="login-verify-wrapper">
+                    <span class="verify-label">验证码：</span>
+                    <input id="login-verify" name="verifyCode" type="text" placeholder="${ opts.verifyPlaceHolder }">
+                    <!--<img src="/verifycode">-->
+                </label>
+
+                <label class="login-remember-wrapper" style="display: ${ showRemember }">
+                    <span>记住密码：</span>
+                    <input id="login-remember" name="remember" type="checkbox">
+                </label>
+
+                <input id="login-btn" class="login-btn" type="submit" value="${ opts.loginBtnText }">
             </form>
     </div>`
     return tpl
